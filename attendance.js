c29f942295f647c0b9715f73a0c1a250e37a253d
Deborah735968 <okeajibola2006@gmail.com>
2025-10-27 09:06:16 +0100

    another commit

--- attendance.js
+++ attendance.js
@@ -1,13 +1,13 @@
- const attendanceForm = document.getElementById('attendanceForm');
-const nameInput = document.getElementById('studentName');
-const matricInput = document.getElementById('matricNumber');
-const courseSelect = document.getElementById('course');
-const statusSelect = document.getElementById('status');
-const attendanceList = document.getElementById('attendanceList');
+const attendanceForm = document.getElementById("attendanceForm");
+const nameInput = document.getElementById("studentName");
+const matricInput = document.getElementById("matricNumber");
+const courseSelect = document.getElementById("course");
+const statusSelect = document.getElementById("status");
+const attendanceList = document.getElementById("attendanceList");
 
 let listText = [];
 
-attendanceForm.addEventListener('submit', function(event) {
+attendanceForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   let studentName = nameInput.value.trim();
@@ -15,24 +15,31 @@ attendanceForm.addEventListener('submit', function(event) {
   let course = courseSelect.value;
   let status = statusSelect.value;
 
-  if (studentName !== '' && matricNumber !== '' && course !== '' && status !== '') {
+  if (
+    studentName !== "" &&
+    matricNumber !== "" &&
+    course !== "" &&
+    status !== ""
+  ) {
     const list = {
       student: studentName,
       matric: matricNumber,
       course: course,
-      status: status
+      status: status,
     };
 
     listText.push(list);
     loadData();
-  } 
+  }
 });
 
 function loadData() {
   let index = listText.length;
-  const newName = document.createElement('li');
+  const newName = document.createElement("li");
 
-  newName.textContent = `${listText[index-1].student} - ${listText[index-1].matric} - ${listText[index-1].course} - ${listText[index-1].status}`;
+  newName.textContent = `${listText[index - 1].student} - ${
+    listText[index - 1].matric
+  } - ${listText[index - 1].course} - ${listText[index - 1].status}`;
 
   attendanceList.appendChild(newName);
   attendanceForm.reset();
